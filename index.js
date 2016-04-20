bdf7a1fa7f8d055ef6c208df8a0b1ab63aa12011
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-04-20 17:02:19 +0100

    First cross platform attempt

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import { NativeModules } from 'react-native';
-module.exports = NativeModules.Sharer;
 
-console.log(NativeModules.Sharer);
-
-// var Sharer = require('react-native').NativeModules.Sharer;
-// module.exports = {
-//     share: function () {
-//         return Sharer.share();
-//     },
-// }
+module.exports = {
+    share: function () {
+        if (Platform.OS === "ios")
+        {
+            ActionSheetIOS.showShareActionSheetWithOptions
+            (
+                {
+                    message: "Donate to my page by texting SMS code to 70070",
+                    excludedActivityTypes: [
+                        'com.apple.UIKit.activity.Message',
+                        'com.apple.UIKit.activity.PostToTwitter',
+                        'com.apple.UIKit.activity.Mail',
+                        'com.apple.UIKit.activity.CopyToPasteboard',
+                        'com.apple.UIKit.activity.AssignToContact',
+                        'com.apple.UIKit.activity.Print',
+                        'com.apple.UIKit.activity.SaveToCameraRoll',
+                        'com.apple.UIKit.activity.PostToFacebook'
+                    ]
+                }
+            );
+        }
+        else
+        {
+            NativeModules.Sharer.share({
+                title: "Share Donation Page",
+                text: "Please donate to my fundraiser via SMS",
+                share_text: "Share Donation SMS number",
+                share_URL: "Please donate to my fundraiser by texting SMS CODE to 70070"
+            });
+        }
+    },
+}
