b7cbcf378849469a9894fc07c8345b7177bb31bf
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-04-20 14:10:38 +0100

    Trying simple way of exporting the module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var Sharer = require('react-native').NativeModules.Sharer;
+var { NativeModules } = require('react-native');
+module.exports = NativeModules.Sharer;
 
-module.exports = {
-    share: function () {
-        return Sharer.share();
-    },
-}
+// var Sharer = require('react-native').NativeModules.Sharer;
+// module.exports = {
+//     share: function () {
+//         return Sharer.share();
+//     },
+// }
