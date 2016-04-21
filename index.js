31778367f113ab0f63918c6f6ef9e8ef6d383abb
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-04-21 15:16:16 +0100

    Fixed resolving promise when actionsheet was dismissed and no option chosen. Added documentation and application to the resolve method.

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ import React, {
 } from "react-native";
 
 module.exports = {
-
+    /**
+     * Share some content with other apps.
+     * @param subject which will be used as email subjects
+     * @param message which you'd like to share
+     * @param url of any external/internal link you'd like to share
+     * @returns A Promise that will be resolved or rejected.
+     *
+     * Rejected promises will contain an _error object with a description of the error.
+     * Resolved promises will contain an application object. This represents what application has been called.
+     *
+     * ON SOME PLATFORMS IT WILL NOT BE POSSIBLE TO FIND OUT WHAT APPLICATION IT HAS BEEN SHARED TO.
+     * FOR THESE PLATFORMS THE APPLICATION WILL BE NULL
+     */
     share: function (subject, message, url) {
-
         return new Promise((resolve, reject) => {
-
             if (typeof subject === 'undefined') {
                 reject({
                     _error: "You must provide a subject to share!"
@@ -34,16 +44,21 @@ module.exports = {
                     {
                         subject: subject,
                         message: message,
-                        url: url,
+                        url: url
                     },
                     (error) => {
                         reject({
                             _error: error
                         });
                     },
-                    (success) => {
-                        resolve({
-                            status: "Fulfilled"
+                    (success, activityType) => {
+                        if (success && activityType) {
+                            resolve({
+                                application: activityType
+                            });
+                        }
+                        reject({
+                            _error: "User did not share"
                         });
                     }
                 );
@@ -55,17 +70,13 @@ module.exports = {
                 });
 
                 resolve({
-                    status: "Promised"
+                    application: null
                 });
-
             } else {
                 reject({
                     _error: "You are trying to use an unsupported platform"
                 });
             }
-
         });
-
     }
-
 }
