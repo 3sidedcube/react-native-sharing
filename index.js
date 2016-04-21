19ad16eb6f5a426a5a7ecb6e505fc6be9575295e
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-04-21 11:11:51 +0100

    Added type checks for parameters. Added success and failure callbacks

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ import React, {
 } from "react-native";
 
 module.exports = {
-    share: function (subject, message, url) {
-        if (Platform.OS === "ios")
-        {
+    share: function (subject, message, url, successCallback, failureCallback) {
+        if (typeof subject === 'undefined') {
+            throw new Error('You must provide a subject to share!');
+        }
+
+        if (typeof message === 'undefined') {
+            throw new Error('You must provide a message to share!');
+        }
+
+        if (typeof url === 'undefined') {
+            throw new Error('You must provide a url to share!');
+        }
+
+        if (typeof successCallback !== 'function') {
+            throw new Error('You must provide a success callback function to share!')
+        }
+
+        if (typeof failureCallback !== 'function') {
+            throw new Error('You must provide a failure callback function to share!');
+        }
+
+        if (Platform.OS === "ios") {
             ActionSheetIOS.showShareActionSheetWithOptions
             (
                 {
@@ -15,20 +34,17 @@ module.exports = {
                     message: message,
                     url: url,
                 },
-                (error) => console.log(error),
-                (success, method) =>
-                {
-                    console.log("Success!");
-                }
+                (error) => failureCallback(),
+                (success) => successCallback()
             );
-        }
-        else
-        {
+        } else if (Platform.OS === "android") {
             NativeModules.Sharer.share({
                 chooser_title: subject,
                 subject: subject,
                 message: message
             });
+        } else {
+            console.log("Trying to use react-native-sharing to share to an unsupported platform: ", Platform.OS);
         }
     },
 }
