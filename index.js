43711012beec6368b6c563a41b16ef226a42784d
Jamie Cruwys <jamie.cruwys@3sidedcube.com>
2016-04-21 14:05:50 +0100

    First attempt at using promises

--- index.js
+++ index.js
@@ -5,46 +5,67 @@ import React, {
 } from "react-native";
 
 module.exports = {
-    share: function (subject, message, url, successCallback, failureCallback) {
-        if (typeof subject === 'undefined') {
-            throw new Error('You must provide a subject to share!');
-        }
 
-        if (typeof message === 'undefined') {
-            throw new Error('You must provide a message to share!');
-        }
+    share: function (subject, message, url) {
 
-        if (typeof url === 'undefined') {
-            throw new Error('You must provide a url to share!');
-        }
+        return new Promise((resolve, reject) => {
 
-        if (typeof successCallback !== 'function') {
-            throw new Error('You must provide a success callback function to share!')
-        }
+            if (typeof subject === 'undefined') {
+                reject({
+                    _error: "You must provide a subject to share!"
+                });
+            }
 
-        if (typeof failureCallback !== 'function') {
-            throw new Error('You must provide a failure callback function to share!');
-        }
+            if (typeof message === 'undefined') {
+                reject({
+                    _error: "You must provide a message to share!"
+                });
+            }
 
-        if (Platform.OS === "ios") {
-            ActionSheetIOS.showShareActionSheetWithOptions
-            (
-                {
+            if (typeof url === 'undefined') {
+                reject({
+                    _error: "You must provide a url to share!"
+                });
+            }
+
+            if (Platform.OS === "ios") {
+                ActionSheetIOS.showShareActionSheetWithOptions
+                (
+                    {
+                        subject: subject,
+                        message: message,
+                        url: url,
+                    },
+                    (error) => {
+                        reject({
+                            _error: error
+                        });
+                    },
+                    (success) => {
+                        resolve({
+                            status: "Fulfilled"
+                        });
+                    }
+                );
+            } else if (Platform.OS === "android") {
+                NativeModules.Sharer.share({
+                    chooser_title: subject,
                     subject: subject,
-                    message: message,
-                    url: url,
-                },
-                (error) => failureCallback(),
-                (success) => successCallback()
-            );
-        } else if (Platform.OS === "android") {
-            NativeModules.Sharer.share({
-                chooser_title: subject,
-                subject: subject,
-                message: message
-            });
-        } else {
-            console.log("Trying to use react-native-sharing to share to an unsupported platform: ", Platform.OS);
-        }
-    },
+                    message: message
+                });
+
+                resolve({
+                    status: "Promised"
+                });
+
+            } else {
+                reject({
+                    _error: "You are trying to use an unsupported platform"
+                });
+            }
+
+        });
+
+    }
+
 }
